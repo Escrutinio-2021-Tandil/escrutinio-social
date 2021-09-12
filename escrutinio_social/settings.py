32a65bd7156fbdea84683ab7e186a048ccb052e5
Facundo Ramos <framos@stormtech.com.ar>
2021-09-12 00:35:35 -0300

    instalacion corsheaders

--- escrutinio_social/settings.py
+++ escrutinio_social/settings.py
@@ -86,11 +86,13 @@ INSTALLED_APPS = [
     'api',
     'antitrolling',
     'scheduling',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -570,3 +572,6 @@ if DEBUG and USAR_DJANGO_DEBUG_TOOLBAR:
 OCULTAR_CANTIDADES_DE_ELECTORES = False
 
 DEFAULT_BACKGROUND_IMAGE = 'img/default_background.jpg'   # relativo a static
+
+
+CORS_ORIGIN_ALLOW_ALL = True
